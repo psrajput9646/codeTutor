05ca581f8ad5609f48fd7b9fdc6d6a335c63aaa7
Bailey Henson <bailey.henson@fotlinc.com>
2019-01-25 18:44:53 -0600

    setup database, sequelize basics

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: process.env.SESSION_SECRET, cookie: { maxAge: 60000}, resave: false, saveUninitialized: false }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
