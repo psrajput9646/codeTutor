a78e29d7dfd19321cb36eee9e7f3a9879c8cfc7a
pharlan97 <peterjharlan@gmail.com>
2019-02-04 09:43:26 -0600

    update account information form input

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,6 +1,6 @@
 // Allows the user to sign up for an account
 import React, { Component } from 'react';
-import { FormGroup, Label, Input, Button } from 'reactstrap';
+import { FormGroup, Label, Input, Button, Row, Col} from 'reactstrap';
 
 export default class SignUp extends Component {
     render() {
@@ -9,14 +9,50 @@ export default class SignUp extends Component {
             <form>
                 <h3 className="mt-3">Sign Up</h3>
                 <FormGroup className="mt-3">
-                    <Label for="exampleEmail">Email</Label>
+                    <Label for="firstname">Name</Label>
+                    <Row>
+                        <Col md={6} sm={12}>
+                            <Input type="text" name="firstName" id="fname" placeholder="First Name"/>
+                        </Col>
+                        <Col md={6} sm={12}>
+                            <Input type="text" className="mt-3 mt-md-0" name="lastname" id="lname" placeholder="Last Name"/>
+                        </Col>
+                    </Row>
+                </FormGroup>
+                <FormGroup className="mt-3">
+                    <Label for="userName">Username</Label>
+                    <Input type="text" name="userName" id="userName" placeholder="Username"/>
+                </FormGroup>
+                <FormGroup className="mt-3">
+                    <Label for="email">Email</Label>
                     <Input type="email" name="email" id="email" placeholder="Email"/>
                     <Input className = "mt-3" type="email" name="emailConfirm" id="emailConfirm" placeholder="Email Confirm"/>
                 </FormGroup>
                 <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input type="password" name="password" id="password" placeholder="password"/>
+                    <Label for="password">Password</Label>
+                    <Input type="password" name="password" id="password" placeholder="Password"/>
                     <Input className = "mt-3" type="password" name="passwordConfirm" id="passwordConfirm" placeholder="Password Confirm"/>
+                </FormGroup>
+                <FormGroup tag="accountType">
+                    <Label for="accountType">Account Status</Label>
+                    <Row>
+                        <Col sm={6}>
+                            <FormGroup check>
+                                <Label check>
+                                <Input type="radio" name="accountStatus" value="Student"/>{' '}
+                                    Student
+                                </Label>
+                            </FormGroup>
+                        </Col>
+                        <Col sm={6}>
+                            <FormGroup check>
+                                <Label check>
+                                <Input type="radio" name="accountStatus" value="Teacher"/>{' '}
+                                    Teacher
+                                </Label>
+                            </FormGroup>
+                        </Col>
+                    </Row>
                 </FormGroup>
             </form>
             <Button color="success" type="submit" className="float-right mb-5">Sign Up</Button>{' '}
