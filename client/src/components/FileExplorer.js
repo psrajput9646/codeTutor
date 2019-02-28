b398c52ac42b16bef258509caafa8345ca7126f6
tduvall <47044650+tduvall@users.noreply.github.com>
2019-02-28 09:02:04 -0600

    Added popup to create new project

--- client/src/components/FileExplorer.js
+++ client/src/components/FileExplorer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Label, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Label, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input } from 'reactstrap';
 import ProjectFile from './ProjectFile';
 
 export default class FileExplorer extends Component {
@@ -7,41 +7,44 @@ export default class FileExplorer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            modal: false
+        modal: false
         };
     
         this.toggle = this.toggle.bind(this);
     }
-      
+    
     toggle() {
         this.setState(prevState => ({
-            modal: !prevState.modal
+        modal: !prevState.modal
         }));
     }
-    
+
     render() {
         return (
         <div>
             <div>
                 <Label for="scriptArea" className="mb-3">Script Input</Label>
 
-                {/* Buttons for script inputs */}
-                <div>
-                Add a Project
-                <Button color="success" size="sm" className="float-right" onClick={this.toggle}>
-                    <i className="fa fa-plus" aria-hidden="true"></i>
-                </Button>{' '}
+                {/* Popup form to create a new project */}
+                <Button color="success" onClick={this.toggle} className="float-right" size="sm">
+                <i className="fa fa-plus" aria-hidden="true"></i></Button>
                 <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                    <ModalHeader toggle={this.toggle}>Create a New Project</ModalHeader>
+                    <ModalHeader toggle={this.toggle}>Add a Project</ModalHeader>
                     <ModalBody>
-                        Insert some form here
+                        <Form>
+                            <FormGroup>
+                                <Label for="projectName">Project Name</Label>
+                                <Input type="text" id="projectName"></Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="description">Description (optional)</Label>
+                                <Input type="textarea" rows="4" id="description"></Input>
+                            </FormGroup>
+                            <Button color="primary" onClick={this.toggle}>Submit</Button>{' '}
+                        </Form>
                     </ModalBody>
-                    <ModalFooter>
-                        Insert submit and cancel buttons here
-                    </ModalFooter>
                 </Modal>
-                </div>
-                <br/>
+
                 <div className="round-div bg-white py-2 pl-2 border">
                     <div className="list-box">
                         <div id="projectOne">
