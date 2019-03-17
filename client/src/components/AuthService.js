a6117d1418ed1ab91e02cc5a876001be791236f8
pharlan97 <peterjharlan@gmail.com>
2019-03-16 23:53:39 -0500

    merge changes

--- client/src/components/AuthService.js
+++ client/src/components/AuthService.js
@@ -2,78 +2,80 @@
 import decode from 'jwt-decode';
 
 export default class AuthService {
+  constructor() {
+    this.fetchAuth = this.fetchAuth.bind(this)
+    this.getProfile = this.getProfile.bind(this)
+    this.isLoggedIn = this.isLoggedIn.bind(this)
+  }
 
-    constructor() {
-        this.fetchAuth = this.fetchAuth.bind(this)
-        this.getProfile = this.getProfile.bind(this)
-        this.isLoggedIn = this.isLoggedIn.bind(this)
+  isLoggedIn = () => {
+    // Checks if there is a saved token and if it is still valid
+    const token = this.getToken() // get token from local storage
+    return !!token && !this.isTokenExpired(token)
+  }
+
+  isTokenExpired = token => {
+    try {
+      const decoded = decode(token)
+      if (decoded.exp < Date.now() / 1000) {
+        return true
+      } else return false
+    } catch (err) {
+      return false
     }
+  }
 
-    isLoggedIn = () => {
-        // Checks if there is a saved token and if it is still valid
-        const token = this.getToken() // get token from local storage
-        return !!token && !this.isTokenExpired(token)
-    }
+  setToken = token => {
+    // Saves user token to local storage
+    localStorage.setItem('user_token', token)
+  }
 
-    isTokenExpired = (token) => {
-        try {
-            const decoded = decode(token);
-            if (decoded.exp < Date.now() / 1000) {
-                return true;
-            } else return false;
-        }
-        catch(err) {
-            return false;
-        }
-    }
+  getToken = () => {
+    // Retrieves the user token from local storage
+    return localStorage.getItem('user_token')
+  }
 
-    setToken = (token) => {
-        // Saves user token to local storage
-        localStorage.setItem('user_token', token)
-    }
+  logout = () => {
+    localStorage.removeItem('user_token')
+  }
 
-    getToken = () => {
-        // Retrieves the user token from local storage
-        return localStorage.getItem('user_token')
-    }
+  getProfile = () => {
+    // Decode the token
+    return decode(this.getToken())
+  }
 
-    logout = () => {
-        localStorage.removeItem('user_token')
-    }
+  fetchAuth = (url, options) => {
+    return new Promise((resolve, reject) => {
+      // performs api calls sending the required authentication headers
+      const headers = {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
 
-    getProfile = () => {
-        // Decode the token
-        return decode(this.getToken());
-    }
+      // Set authorization header
+      if (this.isLoggedIn()) {
+        headers['x-access-token'] = this.getToken()
+      }
 
-    fetchAuth = (url, options) => {
-        // performs api calls sending the required authentication headers
-        const headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-
-        // Set authorization header
-        if (this.isLoggedIn()) {
-            headers['x-access-token'] = this.getToken()
-        }
-
-        return fetch(url, {
-            headers,
-            ...options
+      return fetch(url, {
+        headers,
+        ...options
+      })
+        .then(res => {
+          if (!res.ok) {
+            res
+              .json()
+              .then(response => {
+                throw response
+              })
+              .catch(err => reject(err.error))
+          } else {
+            res.json().then(response => {
+              resolve(response)
+            })
+          }
         })
-        .then(handleErrors)
-        .then(response => {
-            return response.json()
-        })
-        .catch((err) => {console.log(err)})
-        
-    }
-}
-
-function handleErrors(response) {
-    if (!response.ok) {
-        throw Error(response.statusText);
-    }
-    return response;
+        .catch(err => reject(err.error))
+    })
+  }
 }
--- server/controllers/project.js
+++ server/controllers/project.js
@@ -1,8 +1,19 @@
 const Project = require("../models").Project;
 const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
 
 module.exports = {
+  // Requires name for project and student Id (grabbed from token)
+  createRoot(req, res){
+    return File.create({
+        name: req.body.name,
+        path: req.decoded.id + "/",
+    })
+    .then(file => res.status(200).send(file))
+    .catch(err => res.status(400).send(err))
+  },
+
   // Requires name for project and student Id (grabbed from token)
   create(req, res) {
     return Project.create({
