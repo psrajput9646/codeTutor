464b17d7807b2b13ab4904d6fe56074045951887
pedanticprogrammer <30430937+pedanticprogrammer@users.noreply.github.com>
2019-03-20 14:35:32 -0500

    Add Dynamic Projects and Functionality to Button
    Projects on the profile page are now dynamically updated with user content.
    "New+" button now has working functionality to create a nrew project from the profile page.

--- client/src/components/ProjectCollection.js
+++ client/src/components/ProjectCollection.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col, ListGroup, Button, ListGroupItem} from 'reactstrap';
+import { Label, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, TabContent, TabPane, Nav, NavItem, NavLink, Row, Col, ListGroup, Button, ListGroupItem} from 'reactstrap';
 import classnames from 'classnames';
 import ProjectInfo from './ProjectInfo.js';
 import SolutionInfo from './SolutionInfo.js';
+import AuthService from './AuthService';
 
 export default class ProjectCollection extends React.Component {
 constructor(props) {
     super(props);
-
-    this.toggle = this.toggle.bind(this);
     this.state = {
-    activeTab: '1'
+        activeTab: '1',
+        modal: false,
+        projectList: [],
+        projectName: '',
+        description: ''
     };
+    
+    this.toggle = this.toggle.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.Auth = new AuthService();
 }
 
 toggle(tab) {
@@ -22,28 +29,63 @@ toggle(tab) {
     }
 }
 
+toggleModal() {
+    this.setState(prevState => ({
+    modal: !prevState.modal
+    }));
+}
+
 handleClick = () => {
     console.log("Here");
 }
 
+handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+}
+
+getProjects = () =>{
+    let user = this.Auth.getProfile();
+    this.Auth.fetchAuth('/api/project/projects/'+user.id, {
+      method: 'GET'
+    })
+      .then(res => {
+        this.setState({
+            projectList: res
+        });
+      })
+      .catch(err => {
+        console.log(err)
+      })
+}
+
+createProject = () =>{
+    const {projectName, description, projectList} = this.state;
+    let newList = projectList;
+    this.Auth.fetchAuth('/api/project/create', {
+        method: 'POST',
+        body: JSON.stringify({
+            name: projectName,
+            description
+        })
+    })
+    .then(res => {
+        newList.push(res);
+        this.setState({
+            projectList: newList
+        });
+    })
+    .catch(err => {
+        console.log(err)
+    })
+        this.toggleModal();
+}
+
+componentDidMount(){
+    this.getProjects();
+}
+
 render() {
-    const myProjectList = [
-        {   
-            id : 'project1',
-            name : 'My Project Name 1',
-            description : 'script Description 1 Description about the script imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently'
-        },
-        {
-            id : 'project2',
-            name : 'My Project Name 2',
-            description : 'script Description 1 Description about the script imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently'
-        },
-        {
-            id : 'project3',
-            name : 'My Project Name 3',
-            description : 'script Description 1 Description about the script imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently'
-        }
-    ]
+    const { projectName, description, projectList } = this.state;
     const favoritedProjectList = [
         {   
             id : 'favProject1',
@@ -71,7 +113,6 @@ render() {
         }
     ]
     return (
-        
         <div className="mb-4">
             <Nav tabs>
                 <NavItem>
@@ -103,9 +144,38 @@ render() {
                         <ListGroup className="mt-3" flush>
                             <ListGroupItem className="pt-0">
                                 <Button className="float-right" size="sm" color="success"
-                                onClick={this.handleClick}>New <strong>+</strong></Button>{' '}
+                                onClick={this.toggleModal}>New <strong>+</strong></Button>{' '}
                             </ListGroupItem>
-                            {myProjectList.map((project)=>
+                            <Modal isOpen={this.state.modal} toggle={this.toggleModal} className={this.props.className}>
+                                <ModalHeader toggle={this.toggleModal}>Add a Project</ModalHeader>
+                                <ModalBody>
+                                    <Form>
+                                        <FormGroup>
+                                            <Label for="projectName">Project Name</Label>
+                                            <Input 
+                                                type="text"
+                                                name="projectName"
+                                                id="ProjectName"
+                                                value={projectName}
+                                                onChange={this.handleChange}
+                                            ></Input>
+                                        </FormGroup>
+                                        <FormGroup>
+                                            <Label for="description">Description (optional)</Label>
+                                            <Input
+                                                type="textarea"
+                                                name="description"
+                                                id="Description"
+                                                rows="4"
+                                                value={description}
+                                                onChange={this.handleChange}
+                                            ></Input>
+                                        </FormGroup>
+                                        <Button color="primary" onClick={this.createProject}>Submit</Button>{' '}
+                                    </Form>
+                                </ModalBody>
+                            </Modal>
+                            {projectList.map( project =>
                                 <ProjectInfo key = {project.id} {...project}/>
                             )}
                         </ListGroup>
