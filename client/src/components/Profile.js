2b3c2c69eb4b396709bda8bb94c680a653e87bc3
tduvall <47044650+tduvall@users.noreply.github.com>
2019-03-21 16:21:08 -0500

    Added button to edit bio

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Button} from 'reactstrap';
+import { Container, Row, Col, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input, UncontrolledTooltip } from 'reactstrap';
+import AuthService from './AuthService';
 
 function formatName(user){
     return user.firstName + ' ' + user.lastName;
 }
 
 export default class Profile extends Component {
+  
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false,
+            bio: '',
+        };
+    
+        this.toggle = this.toggle.bind(this);
+        this.Auth = new AuthService();
+    }
+    
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+    
+    toggle = () => {
+        this.setState(prevState => ({
+        modal: !prevState.modal
+        }));
+    }
+
     render() {
         const user = this.props;
         return (
@@ -19,19 +42,41 @@ export default class Profile extends Component {
             </Container>
             <h4 className="mt-2"><strong>{formatName(user)}</strong></h4>
             <h6>{user.userName}</h6>
-            <div>
+            <span id="likes">
                 <span className="text-secondary">
                     <i className="fas fa-heart fa text-danger"></i>
                 </span>
                 <span className="font-weight-light pl-2">
                     {user.likes}
                 </span>
-            </div>
+            </span>
+            <UncontrolledTooltip placement="top" target="likes">
+                Total Likes
+            </UncontrolledTooltip>
             <hr className="mb-1"/>
             <p>
                 <small>{user.bio}</small>
             </p>
-            <Button color="secondary" className="btn-sm btn-block">Edit</Button>{' '}
+            <Button color="secondary" className="btn-sm btn-block" onClick={this.toggle}>Edit</Button>{' '}
+            <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                <ModalHeader toggle={this.toggle}>Edit Bio</ModalHeader>
+                <ModalBody>
+                    <Form>
+                        <FormGroup>
+                            <Label for="bio">Bio</Label>
+                            <Input
+                                type="textarea"
+                                name="bio"
+                                id="Bio"
+                                rows="4"
+                                value={user.bio}
+                                onChange={this.handleChange}
+                            ></Input>
+                        </FormGroup>
+                        <Button color="primary">Submit</Button>{' '}
+                    </Form>
+                </ModalBody>
+            </Modal>
         </div>
         )
     }
