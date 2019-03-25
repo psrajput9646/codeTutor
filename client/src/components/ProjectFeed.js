91efe1c07942de9493952de08b52c0a9219739ab
pedanticprogrammer <30430937+pedanticprogrammer@users.noreply.github.com>
2019-03-25 10:59:14 -0500

    Fix key error
    Iterator must have key value.

--- client/src/components/ProjectFeed.js
+++ client/src/components/ProjectFeed.js
@@ -6,17 +6,17 @@ export default class ProjectFeed extends Component {
     render() {
         const newProjectHelpList = [
             {   
-                id : 'HelpProject1',
+                key : 'HelpProject1',
                 name : 'Project Name 1',
                 description : 'script Description 1 Description about the script imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently'
             },
             {
-                id : 'HelpProject2',
+                key : 'HelpProject2',
                 name : 'Project Name 2',
                 description : 'script Description 1 Description about the script imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently'
             },
             {
-                id : 'HelpProject3',
+                key : 'HelpProject3',
                 name : 'Project Name 3',
                 description : 'script Description 1 Description about the script imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently'
             }
