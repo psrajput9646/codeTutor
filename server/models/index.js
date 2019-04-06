7a94b156ccb03319630bfa6e4e22282e7b9bacab
bly01854 <O80696o80696>
2019-04-06 16:07:40 -0500

    Added user login/registration testing

--- server/models/index.js
+++ server/models/index.js
@@ -7,6 +7,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
+config.logging = false;
 const db = {};
 
 let sequelize;
